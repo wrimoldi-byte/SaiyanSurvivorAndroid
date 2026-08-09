4ae33eea31bbdaeff611333bae08e8ad8095a887
wrimoldi-byte <wrimoldi@gmail.com>
2026-08-09 18:23:30 -0300

    Agregar módulo Android

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.walter.saiyansurvivor"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.walter.saiyansurvivor"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
