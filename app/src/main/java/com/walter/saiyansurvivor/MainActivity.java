dd58112f3d886110fd1d54c28ab8760fabc20c4c
wrimoldi-byte <wrimoldi@gmail.com>
2026-08-09 19:15:50 -0300

    Switch app to v0.3.0 game view

--- app/src/main/java/com/walter/saiyansurvivor/MainActivity.java
+++ app/src/main/java/com/walter/saiyansurvivor/MainActivity.java
@@ -11,14 +11,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new GameView(this));
+        setContentView(new GameViewV3(this));
         if (android.os.Build.VERSION.SDK_INT >= 30) {
             getWindow().setDecorFitsSystemWindows(false);
             WindowInsetsController c = getWindow().getInsetsController();
             if (c != null) c.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
         } else {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    5894 | 4096 | 2 | 4);
+            getWindow().getDecorView().setSystemUiVisibility(5894 | 4096 | 2 | 4);
         }
     }
 }
