f5e916ffcd68bceebb7fb9de199868cf1a0db3bc
wrimoldi-byte <wrimoldi@gmail.com>
2026-08-09 18:23:46 -0300

    Agregar actividad principal

---
+++ app/src/main/java/com/walter/saiyansurvivor/MainActivity.java
@@ -0,0 +1,24 @@
+package com.walter.saiyansurvivor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GameView(this));
+        if (android.os.Build.VERSION.SDK_INT >= 30) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController c = getWindow().getInsetsController();
+            if (c != null) c.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    5894 | 4096 | 2 | 4);
+        }
+    }
+}
