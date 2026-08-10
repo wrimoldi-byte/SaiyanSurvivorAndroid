f9507a9efff36b2fc5506cb37ba0cb4a47f7e8b3
wrimoldi-byte <wrimoldi@gmail.com>
2026-08-10 01:02:38 -0300

    Use v0.6.2 game view

--- app/src/main/java/com/walter/saiyansurvivor/MainActivity.java
+++ app/src/main/java/com/walter/saiyansurvivor/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new GameViewV9(this));
+        setContentView(new GameViewV10(this));
         if (android.os.Build.VERSION.SDK_INT >= 30) {
             getWindow().setDecorFitsSystemWindows(false);
             WindowInsetsController c = getWindow().getInsetsController();
