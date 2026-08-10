700408a538c1dfae36a9be301060576709272b97
wrimoldi-byte <wrimoldi@gmail.com>
2026-08-09 22:19:51 -0300

    Bump Saiyan Survivor to v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.walter.saiyansurvivor"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.4.0"
     }
 }
